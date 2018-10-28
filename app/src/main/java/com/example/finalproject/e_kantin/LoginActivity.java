065c1043e83ebe9f9ce2d9a8a30f7d3a7f58b147
gustumaulanaf <gustumaulanaf@gmail.com>
2018-10-28 20:45:55 +0700

    Update Fp 28/10

--- app/src/main/java/com/example/finalproject/e_kantin/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/e_kantin/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.example.finalproject.e_kantin;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/com/example/finalproject/e_kantin/SplashActivity.java
+++ app/src/main/java/com/example/finalproject/e_kantin/SplashActivity.java
@@ -1,18 +1,9 @@
 package com.example.finalproject.e_kantin;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.AnimationDrawable;
-import android.os.Build;
 import android.os.Handler;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
